6aeab5cc6a4cba58de5f739a794f69b1f738ffb5
Gento <a551.return@gmail.com>
2023-05-15 07:40:58 +0900

    テーマを追加

---
+++ lib/theme/palette.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const primaryColor = Color.fromRGBO(255, 255, 255, 1);
+  static const secondaryColor = Color.fromRGBO(135, 165, 209, 1);
+  static const primaryPaleColor = Color.fromRGBO(241, 208, 207, 1);
+  static const secondaryPaleColor = Color.fromRGBO(208, 216, 229, 1);
+}
